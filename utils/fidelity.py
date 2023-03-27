46b9aa68786f90b3b47e5873c0eefc2f975d2b44
Frederico Lopes <frederico.m.lps@gmail.com>
2023-03-27 17:29:38 +0100

    cardiovascular working

--- utils/fidelity.py
+++ utils/fidelity.py
@@ -1,47 +1,21 @@
 from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
 
-def eval_fidelity(X_train, y_train, X_test, X_syn_train, X_syn_test, y_syn_train):
+def get_predictions(X_train, y_train, X_test, y_test):
     learners = [(AdaBoostClassifier(n_estimators=50))]
+    #learners = [(RandomForestClassifier())]
 
     history = dict()
 
     for i in range(len(learners)):
-        model_real = learners[i]
-        model_real.fit(X_train, y_train)
-
-        model_fake = learners[i]
-        model_fake.fit(X_syn_train, y_syn_train)
+        model = learners[i]
+        model.fit(X_train, y_train)
 
         #first letter is data where it trained, second is data where it tested
-        rr_pred = []
-        fr_pred = []
-        ff_pred = []
-        rf_pred = []
-
-        # print(len(X_test))
-        # print(len(X_syn_test))
-
-        # if len(X_test != len(X_syn_test)):
-        #     print("stop")
-        #     break
+        pred = []
 
         for j in range (len(X_test)):
-            rr_pred.append(model_real.predict(X_test.iloc[j]))
-            fr_pred.append(model_fake.predict(X_test.iloc[j]))
-            rf_pred.append(model_fake.predict(X_syn_test.iloc[j]))
-            ff_pred.append(model_fake.predict(X_syn_test.iloc[j]))
+            #print(X_test.loc[[j]])
+            pred.append(model.predict(X_test.iloc[[j]]))
         
-    return rr_pred, fr_pred, ff_pred, rf_pred
-
-        # for index, row in X_test.iterrows():
-        #     rr_pred.append(model_real.predict(row))
-        #     fr_pred.append(model_fake.predict(row))
-        #     # rf_pred.append(model_fake.predict(X_syn_test[j]))
-        #     # ff_pred.append(model_fake.predict(X_syn_test[j]))
-        
-        # for index, row in X_syn_test.iterrows():
-        #     rf_pred.append(model_real.predict(row))
-        #     ff_pred.append(model_fake.predict(row))
-        
-
+    return pred
 
--- utils/privacy.py
+++ utils/privacy.py
@@ -3,16 +3,27 @@ from math import dist
 
 import numpy as np
 
-def get_nearest(src_point, candidates):
-    min_dist = 1000
-    min_index = 0
-    for i in range(len(candidates)):
-        distance = dist(src_point, candidates[i])
-        if distance < min_dist:
-            min_dist = dist
-            min_index = i
+from sklearn.neighbors import BallTree
 
-    return min_index, min_dist
+def get_nearest(src_points, candidates, k_neighbors=1):
+    '''
+    Find nearest neighbors for all source points from a set of candidate points
+    '''
+    # Create tree from the candidate points
+    #knn = NearestNeighbors(n_neighbors=3)
+    tree = BallTree(candidates, leaf_size=15)
+    # Find closest points and distances
+    distances, indices = tree.query(src_points, k=k_neighbors)
+    # Transpose to get distances and indices into arrays
+    distances = distances.transpose()
+    indices = indices.transpose()
+    # Get closest indices and distances (i.e. array at index 0)
+    # note: for the second closest points, you would take index 1, etc.
+    closest = indices[0]
+    closest_dist = distances[0]
+    # Return indices and distances
+    return closest, closest_dist, indices, distances
+
 
 def avg_dist(src_points, candidates):
     sum_dist = []
@@ -21,24 +32,3 @@ def avg_dist(src_points, candidates):
         sum_dist += dist
 
     return sum_dist/i 
-
-    
-
-# def get_nearest(src_points, candidates, k_neighbors=1):
-#     '''
-#     Find nearest neighbors for all source points from a set of candidate points
-#     '''
-#     # Create tree from the candidate points
-#     knn = NearestNeighbors(n_neighbors=3)
-#     #tree = BallTree(candidates, leaf_size=15)
-#     # Find closest points and distances
-#     distances, indices = tree.query(src_points, k=k_neighbors)
-#     # Transpose to get distances and indices into arrays
-#     distances = distances.transpose()
-#     indices = indices.transpose()
-#     # Get closest indices and distances (i.e. array at index 0)
-#     # note: for the second closest points, you would take index 1, etc.
-#     closest = indices[0]
-#     closest_dist = distances[0]
-#     # Return indices and distances
-#     return (closest, closest_dist)
